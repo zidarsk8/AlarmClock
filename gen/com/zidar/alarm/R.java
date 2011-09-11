28450945c174f2c0b67636316aeb92830cc15f60
Smotko <ensmotko@gmail.com>
2011-09-11 14:10:16 +0200

    Added some fake alarms

--- gen/com/zidar/alarm/R.java
+++ gen/com/zidar/alarm/R.java
@@ -25,7 +25,8 @@ public final class R {
     }
     public static final class layout {
         public static final int list=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int list_item=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int AlarmList_add=0x7f040003;
--- src/com/zidar/alarm/AlarmListActivity.java
+++ src/com/zidar/alarm/AlarmListActivity.java
@@ -1,16 +1,18 @@
 package com.zidar.alarm;
 
-import com.zidar.alarm.R;
-
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class AlarmListActivity extends ListActivity {
 
+	static final String[] FAKE_ALARMS = new String[]{ "Fake alarm 8:00", "Fake alarm 10:00", "Fake alarm 9:00", "Fake alarm 1:00", "Fake alarm 12:00", "Fake alarm 10:00", "Fake alarm 9:00", "Fake alarm 1:00", "Fake alarm 12:00", "Fake alarm 10:00", "Fake alarm 9:00", "Fake alarm 1:00", "Fake alarm 12:00" };
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list);
+		
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, FAKE_ALARMS));
 	}
 }
