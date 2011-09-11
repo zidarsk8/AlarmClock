8ae63ca2972f39ee84c131f829c601af87509bb0
Smotko <ensmotko@gmail.com>
2011-09-11 23:57:22 +0200

    View holder seems to be working

--- src/com/zidar/alarm/AlarmListActivity.java
+++ src/com/zidar/alarm/AlarmListActivity.java
@@ -1,12 +1,18 @@
 package com.zidar.alarm;
 
+import java.util.ArrayList;
+
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AlarmListActivity extends ListActivity {
 
@@ -17,7 +23,7 @@ public class AlarmListActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list);
 		
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, FAKE_ALARMS));
+		setListAdapter(new MyAdapter(this, R.layout.list_item, FAKE_ALARMS));
 		
 		Button add = (Button)findViewById(R.id.add_alarm);
 		add.setOnClickListener(new OnClickListener() {
@@ -28,4 +34,44 @@ public class AlarmListActivity extends ListActivity {
 			}
 		});
 	}
+	
+	static class ViewHolder{
+		
+		private TextView v1;
+		private TextView v2;
+	}
+	
+	private class MyAdapter extends ArrayAdapter<String>{
+		
+		LayoutInflater vi = (LayoutInflater) AlarmListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		private final String[] alarms;
+		
+		public MyAdapter(final Context context, final int resId, final String[] alarms){
+			super(context, resId, alarms);
+			this.alarms = alarms;
+		}
+		@Override
+		public View getView(final int position, final View convertView, final ViewGroup parent){
+			
+			View v = convertView;
+			if (v == null){
+				
+				v = vi.inflate(R.layout.list_item, null);
+				ViewHolder vh = new ViewHolder();
+				vh.v1 = (TextView) v.findViewById(R.id.list_item_text);
+				v.setTag(vh);
+			}
+			String a = alarms[position];
+			if (a != null){
+				
+				ViewHolder vh = (ViewHolder) v.getTag();
+				vh.v1.setText(a);
+				
+			}
+			return v;
+		}
+		
+	}
+	
 }
+
